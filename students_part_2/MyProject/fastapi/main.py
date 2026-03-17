76ebb5d96f5b210e41d597bd6e70b22a014c7eb4
Yehonatan Bloy <yehonatan.bloy@gmail.com>
2026-03-17 18:48:34 +0200

    stop

--- students_part_2/MyProject/fastapi/main.py
+++ students_part_2/MyProject/fastapi/main.py
@@ -13,36 +13,108 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-s = my_data.connection_data_base()
-if s == -1:
-    logger.info(11111111111111111)
 
 
 @app.get("/")
 async def root():
     return {"message":"hello user"}
 
+@app.get("/show_all_table")
+async def show_all_table():
+    data = my_data.show_all_table()
+    logger.info(data)
+    return data
+
 @app.get("/select_all_damage_assessments")
 async def select_all_damage_assessments():
-    return my_data.select_all_damage_assessments()
+    data = my_data.select_all_damage_assessments()
+    # logger.info(data)
+    return data
+
+@app.get("/show_all_damage_assessments")
+async def show_all_damage_assessments():
+    data = my_data.show_all_damage_assessments()
+    logger.info(data)
+    return data
 
 @app.get("/select_all_attacks")
 async def select_all_attacks():
-    return my_data.select_all_attacks()
+    data = my_data.select_all_attacks()
+    # logger.info(data)
+    return data
+
+@app.get("/show_all_attacks")
+async def show_all_attacks():
+    data = my_data.show_all_attacks()
+    # logger.info(data)
+    return data
+
 
 @app.get("/select_all_targets")
 async def select_all_targets():
-    return my_data.select_all_targets()
+    data = my_data.select_all_targets()
+    # logger.info(data)
+    return data
+
+@app.get("/show_all_targets")
+async def show_all_targets():
+    data = my_data.show_all_targets()
+    # logger.info(data)
+    return data
+
 
 @app.get("/select_all_intel_signals")
 async def select_all_intel_signals():
-    return my_data.select_all_intel_signals()
+    data = my_data.select_all_intel_signals()
+    # logger.info(data)
+    return data
+
+@app.get("/show_all_intel_signals")
+async def show_all_intel_signals():
+    data = my_data.show_all_intel_signals()
+    # logger.info(data)
+    return data
 
 
 
+@app.get("/select_1")
+async def select_1():
+    data = my_data.select_1()
+    # logger.info(data)
+    return data
 
 
 
+@app.get("/select_2")
+async def select_2():
+    data = my_data.select_2()
+    # logger.info(data)
+    return data
+
+@app.get("/select_3")
+async def select_3():
+    data = my_data.select_3()
+    # logger.info(data)
+    return data
+
+@app.get("/select_4")
+async def select_4():
+    data = my_data.select_4()
+    logger.info(data)
+    return data
+
+
+@app.get("/select_5")
+async def select_5():
+    list_all_coordinates_and_entity_id = []
+    list_coordinates_and_entity_id = []
+    list_entity_id = my_data.return_list_entity_id()
+    for entity_id in list_entity_id:
+        coordinates = my_data.select_5(entity_id)
+        list_coordinates_and_entity_id.append(coordinates)
+        list_coordinates_and_entity_id.append(entity_id)
+        list_all_coordinates_and_entity_id.append(list_coordinates_and_entity_id)
+    return list_all_coordinates_and_entity_id
 
 
 
--- students_part_2/MyProject/fastapi/my_data.py
+++ students_part_2/MyProject/fastapi/my_data.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import logging
+import json
+from maps_data.DigitalHunter_map import plot_map_with_geometry
 
 logging.basicConfig(
     level=logging.INFO,  # DEBUG, INFO, WARNING, ERROR, CRITICAL
@@ -24,34 +26,139 @@ def connection_data_base():
 
 mydb = connection_data_base()
 if mydb == -1:
-    logger.info("no data base")
+    logger.info("I'm sorry but there is no data base")
+    Exception("I'm sorry but there is no data base")
 
 
 
 
 
-def select_all_attacks():
-    mytabol = mydb.cursor()
-    mytabol.execute("SELECT * FROM attacks")
-    myresult = mytabol.fetchall()
-    return myresult
-
-def select_all_damage_assessments():
-    mytabol = mydb.cursor()
-    mytabol.execute("SELECT * FROM damage_assessments")
-    myresult = mytabol.fetchall()
-    return myresult
-
-
-def select_all_targets():
+def show_all_table():
     my_table = mydb.cursor()
-    my_table.execute("SELECT * FROM targets")
+    my_table.execute("SHOW TABLES;")
     result = my_table.fetchall()
     return result
 
 
+def select_all_attacks():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT * FROM attacks;")
+    result = my_table.fetchall()
+    return result
+
+def show_all_attacks():
+    my_table = mydb.cursor()
+    my_table.execute("SHOW COLUMNS FROM attacks;")
+    result = my_table.fetchall()
+    return result
+
+
+
+def select_all_damage_assessments():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT * FROM damage_assessments;")
+    result = my_table.fetchall()
+    return result
+
+def show_all_damage_assessments():
+    my_table = mydb.cursor()
+    my_table.execute("SHOW COLUMNS FROM damage_assessments;")
+    result = my_table.fetchall()
+    return result
+
+
+
+
+def select_all_targets():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT * FROM targets;")
+    result = my_table.fetchall()
+    return result
+
+def show_all_targets():
+    my_table = mydb.cursor()
+    my_table.execute("SHOW COLUMNS FROM targets;")
+    result = my_table.fetchall()
+    return result
+
+
+
+
 def select_all_intel_signals():
-    mytabol = mydb.cursor()
-    mytabol.execute("SELECT * FROM intel_signals")
-    myresult = mytabol.fetchall()
-    return myresult
+    my_table = mydb.cursor()
+    my_table.execute("SELECT * FROM intel_signals;")
+    result = my_table.fetchall()
+    return result
+
+def show_all_intel_signals():
+    my_table = mydb.cursor()
+    my_table.execute("SHOW COLUMNS FROM intel_signals;")
+    result = my_table.fetchall()
+    return result
+
+
+
+def select_1():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT entity_id, target_name, priority_level FROM targets WHERE priority_level IN (1,2)\
+     AND movement_distance_km > 5;")
+    result = my_table.fetchall()
+    return result
+
+
+def select_2():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT signal_type, COUNT(*) AS count FROM intel_signals\
+     GROUP BY signal_type ORDER BY count DESC;")
+    result = my_table.fetchall()
+    return result
+
+
+def select_3():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT entity_id, COUNT(*) AS count FROM intel_signals\
+     WHERE priority_level = 99 GROUP BY entity_id ORDER BY count DESC LIMIT 3;")
+    result = my_table.fetchall()
+    return result
+
+
+def select_4():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT entity_id\
+     FROM intel_signals GROUP BY entity_id\
+      HAVING MAX(CASE WHEN HOUR(timestamp) BETWEEN 8 AND 19 THEN distance_from_last ELSE 0 END)=0\
+       AND MAX(CASE WHEN HOUR(timestamp) < 8 or HOUR(timestamp) >=20 THEN distance_from_last ELSE 0 END)>10;")
+    result = my_table.fetchall()
+    return result
+
+
+def return_list_entity_id():
+    my_table = mydb.cursor()
+    my_table.execute("SELECT entity_id FROM\
+         intel_signals ORDER BY entity_id;")
+    result = my_table.fetchall()
+    return result
+
+def select_5(entity_id):
+    my_table = mydb.cursor()
+    my_table.execute("SELECT entity_id, reported_lat, reported_lon FROM\
+     intel_signals ORDER BY entity_id;")
+    result = my_table.fetchall()
+    list_coordinates_entity_id = []
+    try:
+        for index in range(len(result)):
+            coordinates_and_entity_id_in_result = result[index]
+            entity_id_in_result = coordinates_and_entity_id_in_result[0]
+            if entity_id_in_result == entity_id:
+                coordinates = (coordinates_and_entity_id_in_result[1],coordinates_and_entity_id_in_result[2])
+                logger.info(coordinates)
+                list_coordinates_entity_id.append(coordinates)
+        logger.info(list_coordinates_entity_id)
+        plot_map_with_geometry(list_coordinates_entity_id)
+    except Exception as e:
+        logger.info(e)
+    return list_coordinates_entity_id
+
+
+
+
